ef86332844b7582d4ad9cbde895b2a1ff340681a
Iryna <iryna.shulim@gmail.com>
2021-07-23 07:58:03 -0500

    practice on object methods

--- js-objects/script.js
+++ js-objects/script.js
@@ -29,3 +29,36 @@
 console.log("the notebook object:", notebook);
 console.log("the pageNum value:", notebook.pageNum);
 console.log("the pageNum value:", notebook["pageNum"]);
+
+/**
+ * Practice: Making methods
+ *
+ * - Create a method for object property color.
+ * - The method receives a value to match the property to be changed.
+ * - Create a simple function to replace the current property value with the received value.
+ * - Test the method by sending new values and checking the properties in the console.
+ */
+
+ const backpack = {
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    newColor: function (colorStatus) {
+      this.color = colorStatus
+    },
+    pocketNum: 15,
+    strapLength: {
+      left: 26,
+      right: 26,
+    },
+    toggleLid: function (lidStatus) {
+      this.lidOpen = lidStatus;
+    },
+    newStrapLength: function (lengthLeft, lengthRight) {
+      this.strapLength.left = lengthLeft;
+      this.strapLength.right = lengthRight;
+    },
+  };
+  console.log("backpack color before:", backpack.color);
+  backpack.newColor("red");
+  console.log("backpack color after:", backpack.color);
